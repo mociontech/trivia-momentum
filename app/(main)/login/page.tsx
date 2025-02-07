e90759ea4d5ef953a4d6f4e898d1ba385d2744a3
Juan Sebastian Rodriguez <chechomens@gmail.com>
2025-02-07 13:54:57 -0500

    feat: updating login page

--- app/(main)/login/page.tsx
+++ app/(main)/login/page.tsx
@@ -1,115 +1,218 @@
 "use client";
 
-import { useState } from "react";
-import { Be_Vietnam_Pro, Montserrat } from "next/font/google";
-import { useRouter } from "next/navigation";
-import { useUser } from "@/hooks/useUser";
-import { register } from "@/utils/db";
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
 import Loader from "@/components/loader";
+import { useToast } from "@/hooks/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import { configVariables } from "@/configVariables";
 
-const Vietnam = Be_Vietnam_Pro({
-  subsets: ["latin"],
-  weight: ["300", "400", "600"],
-});
+export default function RegisterExperiencePage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentScreen, setCurrentScreen] = useState(1);
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["700", "800"],
-});
+  const [formData, setFormData] = useState({ id1: "", id2: "", score: "" });
+  const [eventParams, setEventParams] = useState(null);
 
-export default function LoginPage() {
-  const router = useRouter();
-  const [nameInput, setNameInput] = useState("");
-  const [emailInput, setEmailInput] = useState("");
-  const [registered, setRegistered] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const { setMail, setLogged } = useUser();
+  const { toast } = useToast();
 
-  async function submitForm() {
+  const [isReadyId, setIsReadyId] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+
+  const id1Ref = useRef(null);
+  const id2Ref = useRef(null);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    if (value.length > 3) return;
+
+    setFormData((prevData) => {
+      const updatedData = { ...prevData, [name]: value.toUpperCase() };
+
+      if (name === "id1" && value.length === 3) {
+        id2Ref.current?.focus();
+      }
+
+      return updatedData;
+    });
+  }
+
+  useEffect(() => {
+    if (currentScreen === 1) {
+      id1Ref.current?.focus();
+    }
+  }, [currentScreen]);
+
+  useEffect(() => {
+    if (formData.id1 !== "" && formData.id2 !== "") setIsReadyId(true);
+
+    const allFieldsFilled = Object.values(formData).every(
+      (field) => field.trim() !== ""
+    );
+    setIsReady(allFieldsFilled);
+  }, [formData]);
+
+  useEffect(() => {
+    async function getEventParams() {
+      const eventParams = await axios.get(
+        `${configVariables.baseUrl}/api/events/check/${configVariables.databaseId}`
+      );
+
+      setEventParams(eventParams.data);
+    }
+
+    getEventParams();
+  }, []);
+
+  function resetForm() {
+    setFormData({ id1: "", id2: "", score: "" });
+  }
+
+  async function registerUserScore() {
+    setIsLoading(true);
+
+    await axios.post(
+      `${configVariables.baseUrl}/api/users/participation/${configVariables.databaseId}/${configVariables.eventName}`,
+      {
+        userId: formData.id1 + formData.id2,
+        newScore: parseInt(formData.score),
+        experienceName: "trivia",
+      }
+    );
+
+    setCurrentScreen(0);
+    resetForm();
+    setIsLoading(false);
+  }
+
+  async function checkUserParticipation() {
     try {
-      // Checkea que ningun campo este vacio
-      if (!nameInput || !emailInput)
-        return alert("Por favor, completa todos los campos");
-      setLoading(true);
+      setIsLoading(true);
 
-      setMail(emailInput); // Guarda el correo
-      setLogged(true);
-      register(nameInput, emailInput);
+      const checkUser = await axios.post(
+        `${configVariables.baseUrl}/api/users/check/${configVariables.databaseId}/${configVariables.eventName}`,
+        {
+          userId: formData.id1 + formData.id2,
+          experienceName: "trivia",
+        }
+      );
 
-      router.push("/trivia");
+      setIsLoading(false);
+
+      if (checkUser.data.notRegistered) {
+        toast({
+          title: "Este ID no existe",
+          description: "Por favor registrate en el evento",
+        });
+
+        return;
+      }
+
+      if (checkUser.data.alreadyRegistered) {
+        toast({
+          title: "Parece que ya participaste en esta experiencia. ¡Gracias!",
+        });
+      } else {
+        if (
+          eventParams &&
+          eventParams.participationExperiences.includes("trivia")
+        ) {
+          await axios.post(
+            `${configVariables.baseUrl}/api/users/participation/${configVariables.databaseId}/${configVariables.eventName}`,
+            {
+              userId: formData.id1 + formData.id2,
+              newScore: eventParams.participationScore,
+              experienceName: "trivia",
+            }
+          );
+          setCurrentScreen(0);
+          resetForm();
+          return;
+        }
+        setCurrentScreen(2);
+      }
     } catch (error) {
-      console.log({ error: error });
+      setIsLoading(false);
+      toast({
+        title: "Ups algo salió mal",
+        description: "Intenta nuevamente",
+      });
+      return;
     }
   }
 
   return (
-    <div className="login h-screen w-screen flex flex-col justify-center items-center relative">
-      {loading && <Loader />}
-
-      <div className="flex flex-col w-auto">
-        <section className="flex flex-col gap-7">
-          <img
-            src="/assets/registrate.svg"
-            alt="Registrate text"
-            className="relative z-50 mb-12 h-[100px]"
-          />
-
-          <div className="relative flex">
-            <label htmlFor="name">
-              <img
-                src="/assets/name.svg"
-                alt="Icono de una persona"
-                className="absolute z-50 text-black/40 top-[30px] left-[68px]"
-              />
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={nameInput}
-              placeholder="Escribe tu nombre"
-              className={`oracle-regular font-normal text-[40px] flex flex-1 h-[110px] w-[855px] pl-[138px] 
-              text-black bg-white/15 rounded-3xl border-[1.5px] border-[#D6544E]`}
-              autoComplete="off"
-              onChange={(e) => {
-                setNameInput(e.target.value);
-              }}
-            />
+    <div className="w-screen h-screen flex justify-center items-center">
+      {currentScreen !== 0 && (
+        <div className="score-background w-screen h-screen flex justify-center items-center">
+          <div className="flex flex-col">
+            {currentScreen === 1 && (
+              <div className="flex flex-col justify-center items-center">
+                <h2 className="telegraf-bold text-white text-[120px]">
+                  Agrega ID
+                </h2>
+                <div className="telegraf-bold flex items-center text-[#DEF44B] text-[80px] gap-3 mb-10">
+                  <input
+                    type="text"
+                    id="id1"
+                    name="id1"
+                    autoComplete="off"
+                    value={formData.id1}
+                    className="w-[230px] bg-white/30 border-[3px] rounded-[16px] text-center pt-3"
+                    onChange={handleChange}
+                    ref={id1Ref}
+                    maxLength={3}
+                  />
+                  <img src="/dash.svg" className="w-[36px] h-[10px]" alt="" />
+                  <input
+                    type="text"
+                    id="id2"
+                    name="id2"
+                    autoComplete="off"
+                    value={formData.id2}
+                    className="w-[230px] bg-white/30 border-[3px] rounded-[16px] text-center pt-3"
+                    onChange={handleChange}
+                    ref={id2Ref}
+                    maxLength={3}
+                  />
+                </div>
+                <button
+                  className="relative top-[60px] w-[850px] h-[100px] text-[50px] bg-transparent "
+                  disabled={!isReadyId}
+                  onClick={checkUserParticipation}
+                ></button>
+              </div>
+            )}
+            {/* {currentScreen === 2 && (
+              <div className="flex flex-col justify-center items-center">
+                <h2 className="telegraf-bold text-white text-[100px]">
+                  ¡Agrega tu puntaje!
+                </h2>
+                <div className="telegraf-bold flex items-center text-[#DEF44B] text-[80px] gap-3 mb-10">
+                  <input
+                    type="number"
+                    id="score"
+                    name="score"
+                    autoComplete="off"
+                    value={formData.score}
+                    className="w-[381px] bg-white/30 border-[3px] rounded-[16px] text-center pt-3"
+                    onChange={handleChange}
+                  />
+                </div>
+                <button
+                  className="telegraf-bold text-[38px] w-[346px] rounded-[16px] bg-[#DEF44B] py-1"
+                  disabled={!isReady}
+                  onClick={registerUserScore}
+                >
+                  Guardar
+                </button>
+              </div>
+            )} */}
           </div>
-          <div className="relative flex">
-            <label htmlFor="email">
-              <img
-                src="/assets/email.svg"
-                alt="Icono de una persona"
-                className="absolute z-50 text-black/40 top-[30px] left-[56px]"
-              />
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={emailInput}
-              className={`oracle-regular font-normal text-[40px] flex flex-1 h-[110px] w-[855px] pl-[138px]
-              text-black bg-white/15 rounded-3xl border-[1.5px] border-[#D6544E]`}
-              placeholder="Escribe tu correo"
-              autoComplete="off"
-              onChange={(e) => {
-                setEmailInput(e.target.value);
-              }}
-            />
-          </div>
-          {registered && (
-            <p className="relative oracle-regular text-[#D6544E] z-50 text-[48px]">
-              ¡Ya has participado!
-            </p>
-          )}
-        </section>
-        <button
-          className={`${montserrat.className} relative z-50 flex justify-center items-center text-3xl px-10 py-16 
-        bg-[#D6544E] text-white h-[48px] text-center text-[50px] rounded-3xl mt-[40px]`}
-          onClick={submitForm}
-        >
-          <img src="/assets/juega-ahora.svg" alt="juega ahora text" />
-        </button>
-      </div>
+        </div>
+      )}
+      {isLoading && <Loader />}
+      <Toaster />
     </div>
   );
 }
---
+++ configVariables.ts
@@ -0,0 +1,7 @@
+export const configVariables = {
+    eventName: "test",
+    databaseId: "eqYZ8Y0NINWl",
+    // baseUrl: "http://127.0.0.1:8080",
+    baseUrl: "https://mini-datahub.onrender.com",
+  };
+  
