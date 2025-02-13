a8a3b2737e732caeba09a6b0c7a2e166d9b08255
Rosyec <chechomens@gmail.com>
2025-02-13 07:44:14 -0500

    feat: updating name event

--- utils/db.ts
+++ utils/db.ts
@@ -72,7 +72,7 @@ export async function getRecords(): Promise<Record[]> {
 export async function getRanking(): Promise<UserKavak[]> {
   try {
     const result = await $axios.post<UserKavak[]>(
-      `/api/ranking/${configVariables.databaseId}/test/trivia`,
+      `/api/ranking/${configVariables.databaseId}/${configVariables.eventName}/trivia`,
       {
         listQty: 10,
       }
@@ -86,7 +86,7 @@ export async function getRanking(): Promise<UserKavak[]> {
 export async function saveScore(userId: string, score: number, time: number) {
   try {
     const result = await $axios.post(
-      `/api/users/participation/${configVariables.databaseId}/test`,
+      `/api/users/participation/${configVariables.databaseId}/${configVariables.eventName}`,
       {
         userId,
         newScore: score,
