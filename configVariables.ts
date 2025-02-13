6945657279911061eb32013fdb79fc7066a396a0
Rosyec <chechomens@gmail.com>
2025-02-13 07:35:32 -0500

    feat: changing name event

--- configVariables.ts
+++ configVariables.ts
@@ -1,5 +1,5 @@
 export const configVariables = {
-    eventName: "test",
+    eventName: "activacion1",
     databaseId: "h0VxSpwLHsdi",
     // baseUrl: "http://127.0.0.1:8080",
     baseUrl: "https://mini-datahub.onrender.com",
