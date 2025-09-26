03ec2a3f7fc3ecee4c4858e3107e6cf92a9abde3
apt-install-mcn <moosescn20@gmail.com>
2025-09-26 00:35:37 -0500

    build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
