e6efd04ba844278a47a70dd28068d80751d76733
apt-install-mcn <moosescn20@gmail.com>
2025-09-26 23:49:17 -0500

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -319,7 +319,7 @@ async function syncOfflineData(): Promise<void> {
   for (const data of pendingData) {
     try {
       const payload = {
-        eventExperienceId: "68516b60-6583-4cbc-ab1b-e224218bc2c4",
+        eventExperienceId: "9d07c492-dd4f-4f55-8be7-c840e7ea9060",
         email: data.email,
         play_timestamp: data.timestamp,
         score: data.score,
@@ -369,7 +369,7 @@ export async function sendUserDataToEvius(email: string, score?: number, bonusSc
     const localId = generateUUID();
     
     const payload = {
-      eventExperienceId: "68516b60-6583-4cbc-ab1b-e224218bc2c4",
+      eventExperienceId: "9d07c492-dd4f-4f55-8be7-c840e7ea9060",
       email: email,
       play_timestamp: playTimestamp,
       score: score || 25,
